3e9fb4790ec550f831557264a2586dbff0d68aca
Vishal <vishuvrm96@gmail.com>
2022-03-26 23:53:31 +0530

    Added a scheduler to clear database

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from Scrap2Infinity.app import app, UploadData
+from Scrap2Infinity.app import app, UploadData, db
 from flask_apscheduler import APScheduler
 #
 # # initialize scheduler
@@ -6,12 +6,13 @@ scheduler = APScheduler()
 scheduler.api_enabled = True
 scheduler.init_app(app)
 
-@scheduler.task("interval", id="auto_uploads_clear",minutes=5, misfire_grace_time=None)
+@scheduler.task("interval", id="auto_uploads_clear",minutes=1, misfire_grace_time=None)
 def auto_uploads_clear():
     for file in os.listdir(app.config['UPLOAD_FOLDER']):
         os.remove(os.path.join(os.path.abspath(app.config['UPLOAD_FOLDER']), file))
 
     UploadData.query.filter_by().delete()
+    db.session.commit()
 
     print("Cleared all files!")
 
