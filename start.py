c7107458546e74984a00a385db20334fa120d8bb
Vishal <vishuvrm96@gmail.com>
2022-03-26 22:13:55 +0530

    testing

--- start.py
+++ start.py
@@ -1,18 +1,18 @@
 from Scrap2Infinity.app import app
-from flask_apscheduler import APScheduler
-
-# initialize scheduler
-scheduler = APScheduler()
-scheduler.api_enabled = True
-scheduler.init_app(app)
-
-@scheduler.task("interval", id="auto_uploads_clear",minutes=60, misfire_grace_time=None)
-def auto_uploads_clear():
-    for file in os.listdir(app.config['UPLOAD_FOLDER']):
-        os.remove(os.path.join(os.path.abspath(app.config['UPLOAD_FOLDER']), file))
-    print("Cleared all files!")
-
-scheduler.start()
+# from flask_apscheduler import APScheduler
+#
+# # initialize scheduler
+# scheduler = APScheduler()
+# scheduler.api_enabled = True
+# scheduler.init_app(app)
+#
+# @scheduler.task("interval", id="auto_uploads_clear",minutes=60, misfire_grace_time=None)
+# def auto_uploads_clear():
+#     for file in os.listdir(app.config['UPLOAD_FOLDER']):
+#         os.remove(os.path.join(os.path.abspath(app.config['UPLOAD_FOLDER']), file))
+#     print("Cleared all files!")
+#
+# scheduler.start()
 
 
 if __name__ == "__main__":
